dc15319451baea1f788f2c53d6c1a175a33a5288
Jordi Petit <jpetit@cs.upc.edu>
2023-06-09 09:33:44 +0200

    Release 1.2.2

--- upload.sh
+++ upload.sh
@@ -14,4 +14,4 @@ git push
 git tag $version -m "Release $version"
 git push origin "$version"
 python3 setup.py sdist bdist_wheel
-python3 -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+python3 -m twine upload --verbose --repository-url https://upload.pypi.org/legacy/ dist/*
